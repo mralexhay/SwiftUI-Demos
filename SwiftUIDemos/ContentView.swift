a26b4c04ca8a2228fce9bd7fcda2ec82b7073343
Alex Hay <mralexhay@gmail.com>
2020-11-23 08:46:22 +0000

    Initial Commit

---
+++ SwiftUIDemos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemos
+//
+//  Created by Alex Hay on 23/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemos/SwiftUIDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemosApp.swift
+//  SwiftUIDemos
+//
+//  Created by Alex Hay on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
